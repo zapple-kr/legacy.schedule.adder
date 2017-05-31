f79aefe0d726c0f49b67c1965dfbc00f78d5ab77
taetae <junts87@gmail.com>
2017-05-31 11:22:30 +0900

    README

---
+++ README.md
@@ -0,0 +1,12 @@
+# 스케줄 추가 스크립트
+## Dependencies
+- pymysql
+```
+pip install pymysql
+```
+
+## usage
+schedule_list.txt 에 데이터 입력 후,
+```
+python adder.py
+```
--- adder.py
+++ adder.py
@@ -1,17 +1,17 @@
 import pymysql
 import time
 
-f = open('test.txt', 'r', encoding='utf-8')
+
+def get_mysql_connection():
+    return pymysql.connect(host='211.115.112.13', port=3306, user='zapple',
+                           passwd='zapple2012', db='ads', charset='utf8')
+
+f = open('schedule_list.txt', 'r', encoding='utf-8')
 
 campaign_id = None
 creative_id = None
 so_code = None
 campaign_name = None
-
-
-def get_mysql_connection():
-    return pymysql.connect(host='211.115.112.13', port=3306, user='zapple',
-                           passwd='zapple2012', db='ads', charset='utf8')
 
 conn = get_mysql_connection()
 
@@ -57,8 +57,7 @@ try:
                 (main_operator_id, created, schedule_name, content_id, default_flag, 
                 user_id, start_date, end_date, group_id, priority_flag, 
                 campaign_id)
-                values (%s, %s, %s, %s, '0', 
-                '2', %s, %s, %s, 3, %s)
+                values (%s, %s, %s, %s, '0', '2', %s, %s, %s, 3, %s)
                 '''
                 cursor.execute(insert_schedule_query, (so_code, now, schedule_name, creative_id,
                                                        start_date, end_date, group_id, campaign_id))
@@ -68,9 +67,10 @@ try:
                 last_id = cursor.fetchall()[0][0]
 
                 insert_time_query = '''
-                                INSERT INTO tbl_time (start_hour, end_hour, schedule_id, status)
-                                VALUES (%s, %s, %s, null)
-                                '''
+                INSERT INTO tbl_time (start_hour, end_hour, schedule_id, status)
+                VALUES (%s, %s, %s, null)
+                '''
+
                 cursor.execute(insert_time_query, (start_time, end_time, last_id))
                 conn.commit()
 
